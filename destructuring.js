86b980778081841f67460c35915793ef07b52e0a
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-04 14:58:39 -0600

    destructure

--- destructuring.js
+++ destructuring.js
@@ -22,7 +22,7 @@ var carDetails = {
 */
 
 //Code Here
-
+const {color, make, model, year} = carDetails
 
 
 ////////// PROBLEM 2 //////////
@@ -35,7 +35,7 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
-  
+  const {firstName, lastName, title} = obj
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -54,7 +54,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function totalPopulation(obj){
+  const {utah, california, texas, arizona} = obj
+  return utah + california + texas + arizona  
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -68,7 +71,12 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function ingredients(obj){
+  arr = []
+  const {carb, fat, protein} = obj 
+  arr.push(carb, fat, protein)
+  return arr
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -86,7 +94,9 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function largeNumbers( {first, second, third} ){
+  return Math.min(first, second, third)
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -98,5 +108,14 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function numberGroups( {a, b, c} ){
+  if (a.length > b.length && a.length > c.length){
+    return a 
+  } else {
+    if (b.length >= a.length && b.length >= c.length){
+      return b 
+    } else {
+        return c
+  }
+}}
 
