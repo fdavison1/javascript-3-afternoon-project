011b239d5bc3c23f5adc392005be5b110395f9fd
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-04 22:42:39 -0600

    3

--- nesting.js
+++ nesting.js
@@ -149,8 +149,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
